db0de474c159d60c3ee45a53c1a67c1aa7bba78f
NakulKataria <nakulkofficial@gmail.com>
2026-02-06 07:00:10 +0530

    logic fix in fib

--- Strivers a2z dsa/learnTheBasics/learnBasicRecursion/fibbonaci.js
+++ Strivers a2z dsa/learnTheBasics/learnBasicRecursion/fibbonaci.js
@@ -2,7 +2,7 @@ const readline= require("readline-sync");
 const n = readline.questionInt("enter the no to find fibbonacci :");
 console.log(fib(n));
 function fib(num){
-    if(num ===0|| num===1) return 1;
+    if(num ===0|| num===1) return n;
     return fib(num-1) +fib(num-2);
 
 }
